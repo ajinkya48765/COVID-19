7f2500b3b38b5de8ff1ae5a160cfb7aa7d0ba695
Shivangi Sharma <62565739+ShivangiSharma77@users.noreply.github.com>
2020-05-16 17:14:05 +0530

    final

--- flask_blog/flask_blog.py
+++ flask_blog/flask_blog.py
@@ -4,12 +4,13 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '550e71fa3b4a0a957b395cb0e2f8642f'
 
-@app.route('/')
+
+@app.route('/',methods=['GET','POST'])
 @app.route('/home',methods=['GET','POST'])
 def home():
     form = RForm()
     if form.validate_on_submit():
-        flash('Information Added', category='success')
+        flash('Information Submitted', category='success')
         return redirect(url_for('home'))
     return render_template('home.html', title= 'Covid-19',form=form)
     
